2292f135850baaa5a43551ef90f351c8b9981e1a
Taufik Nurhidayat <fiik346@gmail.com>
2025-04-11 12:39:47 +0700

    init

---
+++ src/lib/components/AppBar.svelte
@@ -0,0 +1,28 @@
+<script>
+	const navs = [
+		{
+			name: 'Home',
+			icon: 'home',
+			link: '/'
+		},
+		{
+			name: 'Bookmark',
+			icon: 'bookmark',
+			link: 'bookmark'
+		}
+	];
+</script>
+
+<div class="bg-base-100 fixed bottom-0 left-0 w-full shadow">
+	<div class="mx-auto flex w-full max-w-xl justify-between gap-2 p-1">
+		{#each navs as nav, index (index)}
+			<a
+				href={nav.link}
+				class="hover:bg-base-200 rounded-field inline-flex grow flex-col items-center justify-center px-4 py-2"
+			>
+				<span class="material-symbols-rounded filled">{nav.icon}</span>
+				<span class="text-xs">{nav.name}</span>
+			</a>
+		{/each}
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import AppBar from '$lib/components/AppBar.svelte';
+	import '../app.css';
+</script>
+
+<div class="h-screen">
+	<div class="h-full overflow-auto">
+		<slot />
+	</div>
+	<AppBar />
+</div>
