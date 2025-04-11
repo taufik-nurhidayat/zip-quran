4e84ec48a4fbe962a97420ed3645de9cffafbb73
Taufik Nurhidayat <fiik346@gmail.com>
2025-04-11 20:40:10 +0700

    Add top border and increase hover contrast on app bar

--- src/lib/components/AppBar.svelte
+++ src/lib/components/AppBar.svelte
@@ -20,12 +20,12 @@
 	];
 </script>
 
-<div class="bg-base-100 fixed bottom-0 left-0 w-full shadow">
+<div class="bg-base-100 border-base-300 fixed bottom-0 left-0 w-full border-t shadow">
 	<div class="mx-auto flex w-full max-w-xl justify-between gap-2 p-1">
 		{#each navs as nav, index (index)}
 			<a
 				href={nav.link}
-				class="hover:bg-base-200 rounded-field inline-flex grow flex-col items-center justify-center px-4 py-2"
+				class="hover:bg-base-300 rounded-field inline-flex grow flex-col items-center justify-center px-4 py-2"
 				class:text-primary={nav.link === page.url.pathname}
 			>
 				<span class="material-symbols-rounded filled">{nav.icon}</span>
