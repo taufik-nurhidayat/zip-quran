c56b1eeb06c7d538fe56b5b680c8f7070d84c42a
Taufik Nurhidayat <fiik346@gmail.com>
2025-04-11 20:23:42 +0700

    Add page title for Zip Qur'an application

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,9 @@
 	import '../icon.css';
 </script>
 
+<svelte:head>
+	<title>Zip Qur'an - Simple Al-Qur'an Applications</title>
+</svelte:head>
 <div class="h-screen">
 	<div class="h-full overflow-auto">
 		<slot />
