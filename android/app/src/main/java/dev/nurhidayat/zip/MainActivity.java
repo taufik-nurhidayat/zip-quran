86f8e516c4682b03935f0db929a8edd55949407a
Taufik Nurhidayat <fiik346@gmail.com>
2025-04-12 07:17:20 +0700

    chore(add): capacitor js for android apps

---
+++ android/app/src/main/java/dev/nurhidayat/zip/MainActivity.java
@@ -0,0 +1,5 @@
+package dev.nurhidayat.zip;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'dev.nurhidayat.zip',
+	appName: 'zip-quran',
+	webDir: 'build'
+};
+
+export default config;
