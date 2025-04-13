066efd665b782550bea78bacebed67064826ac97
Taufik Nurhidayat <fiik346@gmail.com>
2025-04-13 11:03:46 +0700

    Add LastRead component to display last read Quran verse

---
+++ src/lib/components/LastRead.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import * as m from '$lib/paraglide/messages';
+</script>
+
+<div class="rounded-box bg-primary text-primary-content p-4">
+	<div>
+		<h3 class="text-sm">{m.last_read()}</h3>
+	</div>
+	<div class="mt-3">
+		<p class="font-semibold">Al Baqarah</p>
+		<p class="mt-1 text-sm opacity-80">{m.verse()} 125</p>
+	</div>
+</div>
