c03cca223cd8462fa1bd3f6903b5f7846776b55a
Taufik Nurhidayat <fiik346@gmail.com>
2025-04-14 07:28:06 +0700

    style: refactor theme changer into vertical join button

--- src/lib/components/ThemeChanger.svelte
+++ src/lib/components/ThemeChanger.svelte
@@ -5,13 +5,10 @@
 <input type="checkbox" id="theme_changer" class="modal-toggle" />
 <div class="modal modal-bottom sm:modal-middle" role="dialog">
 	<div class="modal-box">
-		<label for="theme_changer" class="btn btn-sm btn-circle btn-ghost absolute top-2 right-2"
-			>✕</label
-		>
 		<h3 class="text-lg font-bold">Change theme</h3>
-		<div class="flex flex-wrap gap-2 py-8">
+		<div class="join join-vertical w-full py-4">
 			{#each availableThemes as value, index (index)}
-				<button class="btn" onclick={() => setTheme(value)}>
+				<button class="btn join-item w-full" onclick={() => setTheme(value)}>
 					<div
 						data-theme={value === 'auto' ? getSystemPreference() : value}
 						class="bg-base-100 grid shrink-0 grid-cols-2 gap-0.5 rounded-md p-1 shadow-sm"
@@ -21,11 +18,14 @@
 						<div class="bg-secondary size-1 rounded-full"></div>
 						<div class="bg-accent size-1 rounded-full"></div>
 					</div>
-					<span class="text-xs uppercase">
+					<span class="flex-1 text-start text-xs uppercase">
 						{value}
 					</span>
 				</button>
 			{/each}
+		</div>
+		<div class="modal-action">
+			<label for="theme_changer" class="btn btn-primary">Save</label>
 		</div>
 	</div>
 	<label class="modal-backdrop" for="theme_changer">close</label>
