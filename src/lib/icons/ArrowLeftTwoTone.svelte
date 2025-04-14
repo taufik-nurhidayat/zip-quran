629bf0c46582a54bb9112d42152300f97a1af312
Taufik Nurhidayat <fiik346@gmail.com>
2025-04-14 07:55:47 +0700

    Add dynamic size support to ArrowLeftTwoTone icon

--- src/lib/icons/ArrowLeftTwoTone.svelte
+++ src/lib/icons/ArrowLeftTwoTone.svelte
@@ -1,4 +1,8 @@
-<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+<script lang="ts">
+	const { size = 24 } = $props();
+</script>
+
+<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width={size} height={size}>
 	<path
 		d="M9.57 5.92969L3.5 11.9997L9.57 18.0697"
 		stroke-width="2"
