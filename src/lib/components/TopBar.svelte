0be4ffb03303f27db8bcc175d1bc05d94f828e6b
Taufik Nurhidayat <fiik346@gmail.com>
2025-04-14 07:48:03 +0700

    refactor: simplify back button and use icon component

--- src/lib/components/TopBar.svelte
+++ src/lib/components/TopBar.svelte
@@ -1,17 +1,8 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import ArrowLeftTwoTone from '$lib/icons/ArrowLeftTwoTone.svelte';
 
-	let canGoback = false;
-
-	onMount(() => {
-		canGoback = window.history.length > 1;
-	});
 	function handleBackClick() {
-		if (canGoback) {
-			window.history.back();
-		} else {
-			window.location.replace('/');
-		}
+		window.history.back();
 	}
 
 	let { title } = $props();
@@ -22,30 +13,7 @@
 		<div class="flex-1">
 			<div class="tooltip tooltip-bottom" data-tip="Back">
 				<button class="btn btn-ghost btn-square" aria-label="Back" onclick={handleBackClick}>
-					<svg
-						viewBox="0 0 24 24"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-					>
-						<path
-							d="M9.57 5.92993L3.5 11.9999L9.57 18.0699"
-							stroke="#292D32"
-							stroke-miterlimit="10"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-						></path>
-						<path
-							d="M20.5 12H3.67004"
-							stroke="#292D32"
-							stroke-miterlimit="10"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-						></path>
-					</svg>
+					<ArrowLeftTwoTone />
 				</button>
 			</div>
 		</div>
