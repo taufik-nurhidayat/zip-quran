90db8c697ef71e3ef644cbcc08f47c21a448e845
Taufik Nurhidayat <fiik346@gmail.com>
2025-04-14 08:01:35 +0700

    style: add stretch link to ayah item

--- src/routes/quran/+page.svelte
+++ src/routes/quran/+page.svelte
@@ -8,8 +8,8 @@
 	<p class="mt-6 text-lg font-semibold">All surah</p>
 	<div class="mt-4">
 		<div class="rounded-box border-base-300 bg-base-100 relative border p-4">
-			<div class="flex items-center gap-4">
-				<div class="">
+			<div class="relative flex items-center gap-4">
+				<div>
 					<div class="avatar bg-base-200 rounded-field">
 						<div class="w-14 text-lg font-bold">
 							<figure class="flex h-full w-full items-center justify-center">1</figure>
@@ -17,7 +17,9 @@
 					</div>
 				</div>
 				<div class="">
-					<h2 class="font-semibold"><a href="/quran/1"> Surah Al-Fatihah </a></h2>
+					<h2 class="font-semibold">
+						<a href="/quran/1" class="stretch-link"> Surah Al-Fatihah </a>
+					</h2>
 					<p class="text-base-content/60 mt-1 text-sm">7 Ayat &bullet; The Opening</p>
 				</div>
 			</div>
